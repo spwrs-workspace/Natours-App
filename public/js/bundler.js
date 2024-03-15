95b7f20224bbe5d7ac24546b87ab214fa7162393
spwrs-workspace <sspwr1502@gmail.com>
2024-03-15 17:42:43 +0530

    Modified welcome pug template

--- public/js/bundler.js
+++ public/js/bundler.js
@@ -12550,7 +12550,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54104" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60774" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
