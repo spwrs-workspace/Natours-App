9c281c6a98f1cea24fdea3e9b2a1cfde3900398a
spwrs-workspace <sspwr1502@gmail.com>
2024-03-16 09:41:39 +0530

    ...

--- public/js/bundler.js
+++ public/js/bundler.js
@@ -12549,7 +12549,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51797" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65251" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
