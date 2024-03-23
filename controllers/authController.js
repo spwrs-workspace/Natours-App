b369e1a41bd38785e892af4b33bb7722dff5c538
Akashp45 <akash.panse@mitaoe.ac.in>
2024-03-23 12:19:08 +0530

    ...

--- controllers/authController.js
+++ controllers/authController.js
@@ -13,9 +13,9 @@ const signToken = (id) => {
 };
 
 const cookieOptions = {
-  expires:
-    new Date(Date.now()) +
-    process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
+  expires: new Date(
+    Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
+  ),
   httpOnly: true,
 
   // maxAge: 3600 * 1000,
